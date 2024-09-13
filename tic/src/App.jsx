8c940f420aaf11f2ce073a80f849faf7c7853358
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-09-14 00:10:24 +0530

    added buttons

--- tic/src/App.jsx
+++ tic/src/App.jsx
@@ -1,12 +1,35 @@
+export  function Board()
+{
+  return(
+    <>
+    <div className="board-ui">
+    <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
+    <div className="board-ui">
+    <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+    </div>
+    <div className="board-ui">
+    <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+    </div>
+    
+    </>
+  )
+  
 
-import './App.css'
+}
 
-function App() {
+export default function App() {
   return (
     <>
- 
+    <Board />
     </>
   )
 }
 
-export default App
+
